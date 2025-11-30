44674abef47871027fe2536a9deb45782e82eaea
OwerdZero0 <franciscocapilla2008@gmail.com>
2025-11-30 15:26:56 -0600

    feat: Introduje traducciones i18n y un nuevo estilo de glassmorphism animado y responsive para la sección `inicio` y actualice `index.html`.

--- Scripts/Traducciones i18n/inicio.js
+++ Scripts/Traducciones i18n/inicio.js
@@ -1,6 +1,11 @@
 export const inicio = {
     es: {
         "titulo_inicio": "Centros de acopio en la comunidad y reciclado",
+
+        "texto_inicio_1": "Este proyecto nos mostró que reciclar no es solo separar residuos, sino descubrir el poder que tenemos para transformar nuestra escuela.",
+        "texto_inicio_2": "A través de gráficas, formularios y datos, aprendimos que cada acción cuenta y que el reciclaje también se aprende creando.",
+        "texto_inicio_3": "Con videos y herramientas digitales, comprendimos que el reciclaje también se comunica, y que nuestra voz puede inspirar a otros a actuar.",
+
         "boton_centro_de_acopio_inicio": "Centro de acopio",
         "boton_reciclaje_inicio": "Reciclaje",
         "boton_galeria_inicio": "Galería",
@@ -11,6 +16,11 @@ export const inicio = {
     },
     en: {
         "titulo_inicio": "Community collection centers and recycling",
+
+        "texto_inicio_1": "This project showed us that recycling is not just separating waste, but discovering the power we have to change our school.",
+        "texto_inicio_2": "Through graphs, forms, and data, we learned that every action matters and that recycling can also be learned by creating.",
+        "texto_inicio_3": "With videos and digital tools, we realized that recycling is also communicated, and that our voice can inspire others to act.",
+
         "boton_centro_de_acopio_inicio": "Collection center",
         "boton_reciclaje_inicio": "Recycling",
         "boton_galeria_inicio": "Gallery",
@@ -21,6 +31,11 @@ export const inicio = {
     },
     nah: {
         "titulo_inicio": "Tianquiztli de tlatlalilizpan ipan altepetl huan tlamaniliztli",
+
+        "texto_inicio_1": "Inin tlamachtilistli technextili queh tlen ticchihua tlamixpolihuilistli ahmo san mitzomana, tlahmo nozo techmachtia tlen ticuīliaz para ticpatlasqueh totlaçamachtiloyan.",
+        "texto_inicio_2": "Ipan amatlacuīcuilolli, tlaixcopincayotl huan tlamachiyalistli, tlamachtiqueh queh sesen tlamantli quipiya ipati, huan tlamixpolihuilistli nozo tlamachtis ipan tlocoyani.",
+        "texto_inicio_3": "Ipan video huan tlaixmatilis tlamantli, tlatepanitac queh tlamixpolihuilistli nozo tlatepanita, huan tohtlahtol huelis quinonotzas ocsequimeh para quichihuazqueh tlen cuali.",
+
         "boton_centro_de_acopio_inicio": "Tianquiztli de tlatlaliliztli",
         "boton_reciclaje_inicio": "Tlamaniliztli occepa tequiti",
         "boton_galeria_inicio": "Tlaixnextiloyan",
